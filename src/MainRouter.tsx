0ad251ae1e58f53b74a3ce83a76ae8ad97d0b951
Kristina Somodi <kristinashomodji@gmail.com>
2022-06-13 09:29:51 +0200

    Login slicing

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import EditQuantity from "./app/features/pages/EditQuantity";
+// import EditQuantity from "./app/features/pages/EditQuantity";
+import Login from "./app/features/pages/Login";
 // import AddProduct from "./app/features/pages/AddProduct";
 // import Products from "./app/features/pages/Products";
 // import Sidebar from "./app/features/components/Sidebar";
@@ -8,7 +9,7 @@ import EditQuantity from "./app/features/pages/EditQuantity";
 function MainRouter() {
   return (
     <Routes>
-      <Route path="/" element={<EditQuantity />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
---
+++ src/app/features/pages/Login.tsx
@@ -0,0 +1,69 @@
+function Login() {
+  return (
+    <>
+      <div className="l">
+        <img
+          src={require("../../../assets/images/background.png")}
+          alt=""
+          className="l--left"
+        />
+
+        <div className="l--right">
+          {/* title */}
+          <div className="title--logo">
+            <div className="title--secondary mt-80 ml-80 mb-42">Login </div>
+            <img
+              src={require("../../../assets/images/logo-xs.png")}
+              alt=""
+              className="title--logo-img ml-24"
+            />
+          </div>
+
+          {/* login form */}
+          <form className="loginForm ml-80 mt-32 flex">
+            <div className="title--form">Login</div>
+
+            {/* inputs, email password */}
+            <div className="field mt-16">
+              <label htmlFor="email" className="field__label ">
+                Email*
+              </label>
+              <input
+                type="text"
+                placeholder="mail@website.com"
+                className="input input--log mt-8"
+                required
+              />
+            </div>
+            <div className="field mt-16">
+              <label htmlFor="password" className="field__label ">
+                Password*
+              </label>
+              <div className="pos--rel">
+                <input
+                  type="password"
+                  placeholder=""
+                  className="input input--log mt-8"
+                  required
+                />
+                <i className="icon icon--base icon--eye-on icon--black input__icon"></i>
+              </div>
+            </div>
+            <div className="type type--montserrat type-500 type-16 type--blue flex--right mt-8">
+              Forgot password?
+            </div>
+            <button className="btn btn--secondary btn--xl mt-80">Login</button>
+          </form>
+          <div className="type type--monsterrat ml-80 mt-24">
+            Don't have an account yet?
+            <span className="type type--monsterrat type-500 type-16 type--blue">
+              Register here.
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
