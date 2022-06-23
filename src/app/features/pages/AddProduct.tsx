c491b70ac0029dd112de971b2417af3fb3b14748
Kristina Somodi <kristinashomodji@gmail.com>
2022-06-23 13:54:19 +0200

    Back button

--- src/app/features/pages/AddProduct.tsx
+++ src/app/features/pages/AddProduct.tsx
@@ -1,14 +1,16 @@
 import AddForm from "../components/AddForm";
 import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
 
 function AddProduct() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="layout">
         <Sidebar></Sidebar>
         <div className="add layout--right">
           <div className="title--secondary mt-32 ml-40 mb-42">Add Product</div>
-          <div className="back ml-40">
+          <div className="back ml-40" onClick={() => navigate(-1)}>
             <i className="icon icon--base icon--chevron icon--blue mr-8"></i>
             Back
           </div>
--- src/app/features/pages/EditQuantity.tsx
+++ src/app/features/pages/EditQuantity.tsx
@@ -1,7 +1,10 @@
 import EditForm from "../components/EditForm";
 import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
 
 function EditQuantity() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="layout">
@@ -10,7 +13,7 @@ function EditQuantity() {
           <div className="title--secondary mt-32 ml-40 mb-42">
             Edit quantity
           </div>
-          <div className="back ml-40">
+          <div className="back ml-40" onClick={() => navigate(-1)}>
             <i className="icon icon--base icon--chevron icon--blue mr-8"></i>
             Back
           </div>
